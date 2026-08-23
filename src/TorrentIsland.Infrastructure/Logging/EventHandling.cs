e81dda3250e2039f455ec684a5fa6ecd2ff91d89
Vitor <vitorzb46@gmail.com>
2026-08-23 12:35:34 -0300

    add: adicionado eventos de encerramento de console.
    removido Task.Delays desnecessários.

--- src/TorrentIsland.Infrastructure/Logging/EventHandling.cs
+++ src/TorrentIsland.Infrastructure/Logging/EventHandling.cs
@@ -6,13 +6,15 @@ using TorrentIsland.Infrastructure.MonoTorrent;
 
 namespace TorrentIsland.Infrastructure.Logging;
 
-public class EventHandling(IManagers managers, ILogger<TorrentRepository> logger) : IEventHandling
+public class EventHandling(IManagers managers, ILogger<TorrentRepository> logger, CancellationToken token = default) : IEventHandling
 {
     private IManagers Managers { get; } = managers;
     private ILogger<TorrentRepository> Logger { get; } = logger;
 
     public async Task EventsAsync(Guid id)
     {
+        Console.WriteLine("Iniciando depêndencias.");
+
         if (!Managers.All.TryGetValue(id, out var manager))
         {
             Logger.LogWarning("Torrent ({TorrentId}) não encontrado.", id);
@@ -31,32 +33,47 @@ public class EventHandling(IManagers managers, ILogger<TorrentRepository> logger
         {
             Logger.LogDebug("{Nome} -> Par conectado: {Peer} ({Direcao}).",
                 Nome(), e.Peer, e.Direction);
-            await Task.Delay(500).ConfigureAwait(false);
         };
 
         manager.PeerDisconnected += async (o, e) =>
         {
             Logger.LogDebug("{Nome} -> Par desconectado: {Peer}.", Nome(), e.Peer);
-            await Task.Delay(500).ConfigureAwait(false);
         };
 
         manager.PieceHashed += async (o, e) =>
         {
             Logger.LogDebug("{Nome} -> Peça {PieceIndex} verificada - passou: {HashPassed} (progresso {Progresso:0.0}%).",
                 Nome(), e.PieceIndex, e.HashPassed, e.Progress);
-            await Task.Delay(4000).ConfigureAwait(false);
         };
 
         manager.ConnectionAttemptFailed += async (o, e) =>
         {
             Logger.LogDebug("{Nome} -> [yellow]Falha de conexão[/] com {Peer}: {Razao}.", Nome(), e.Peer, e.Reason);
-            await Task.Delay(2000).ConfigureAwait(false);
         };
 
         manager.TorrentStateChanged += (o, e) =>
         {
             Logger.LogInformation("{Nome} -> Estado alterado: {Antigo} -> {Novo}",
                 Nome(), e.OldState, e.NewState);
+        };
+
+        // Console
+        AppDomain.CurrentDomain.ProcessExit += async (sender, e) =>
+        {
+            Logger.LogInformation("[red]Encerrando processo...[/]");
+
+            await Task.Delay(2000, token).ConfigureAwait(false);
+        };
+
+        Console.CancelKeyPress += async (sender, e) =>
+        {
+            e.Cancel = true;
+
+            Logger.LogInformation("[red]Ctrl + c pressionado. Aguarde...[/]");
+
+            await Task.Delay(2000, token).ConfigureAwait(false);
+
+            Environment.Exit(0);
         };
     }
 }
