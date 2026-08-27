7a5193aa19fab7f99c7ad01a396ffada014bca62
vitorzb46 <vitorzb46@gmail.com>
2026-08-26 22:38:27 -0300

    add: Dependency Injection no WPF Player
    Puxando método de extensão do application para as dependências.

--- src/TorrentIsland.Presentation.Player/App.xaml.cs
+++ src/TorrentIsland.Presentation.Player/App.xaml.cs
@@ -1,5 +1,19 @@
 using System.IO;
 using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MonoTorrent.Client;
+using TorrentIsland.Application.Contracts;
+using TorrentIsland.Application.Interfaces;
+using TorrentIsland.Application.Medias.Commands;
+using TorrentIsland.Application.Services;
+using TorrentIsland.Application.Settings;
+using TorrentIsland.Domain.Interfaces;
+using TorrentIsland.Infrastructure.DependencyInjection;
+using TorrentIsland.Infrastructure.Interfaces;
+using TorrentIsland.Infrastructure.Logging;
+using TorrentIsland.Infrastructure.MonoTorrent;
+using TorrentIsland.Infrastructure.Services;
 
 namespace TorrentIsland.Presentation.Player;
 
@@ -25,8 +39,24 @@ public partial class App : System.Windows.Application
 
         // Uso: TorrentIsland.Presentation.Player.exe <url-do-stream>
         var mediaUrl = e.Args.Length > 0 ? e.Args[0] : null;
+        
+        var services = new ServiceCollection();
+        ConfigurarServico(services);
+        var serviceProvider = services.BuildServiceProvider();
 
-        var window = new PlayerWindow(mediaUrl!);
+        var window = serviceProvider.GetRequiredService<PlayerWindow>();
+        window.mediaUrl = mediaUrl!;
         window.Show();
+    }
+
+    private void ConfigurarServico(IServiceCollection services)
+    {
+        var configuration = new ConfigurationBuilder()
+        .SetBasePath(AppContext.BaseDirectory)
+        .Build();
+        var consoleLog = services.BuildServiceProvider().GetRequiredService<IConsoleLogRenderer>();
+
+        services.AddTorrentIsland(configuration, consoleLog, maxLogs: 10);
+        services.AddSingleton<PlayerWindow>();
     }
 }
