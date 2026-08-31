593478bd234b9e918a769961acc92743ee2b031a
vitorzb46 <vitorzb46@gmail.com>
2026-08-30 22:24:02 -0300

    nova classe para reunir hashsets e talvez dicionários depois.

---
+++ src/TorrentIsland.Presentation.Player/Utils.cs
@@ -0,0 +1,13 @@
+namespace TorrentIsland.Presentation.Player;
+
+public class Utils
+{
+    public static readonly string[] ExtensoesVideo = [
+        ".mp4", ".mkv", ".avi", ".mov", ".wmv", ".flv", ".webm",
+        ".m4v", ".ts", ".m2ts", ".vob", ".mpg", ".mpeg", ".3gp", ".ogv"
+    ];
+    public static readonly string[] ExtensaoTorrent = [".torrent"];
+    public static readonly string[] ExtensoesSubs = [
+        ".srt", ".vtt", ".ssa", ".ass",
+    ];
+}
