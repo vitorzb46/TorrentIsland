ce9a09621ca78e0e65d0096b20a7bcd2eb7feefd
vitorzb46 <vitorzb46@gmail.com>
2026-09-02 22:19:25 -0300

    feature: Novo botão para buscar torrents
    Função para buscar filmes e séries online, e retorna uma lista com maiores seeds em ordem.
    Procura na web pela primeira vez e salva cache em disco se fizer a mesma pesquisa novamente.

---
+++ src/TorrentIsland.Presentation.Player/TorrentSearchWindow.xaml.cs
@@ -0,0 +1,56 @@
+using System.Windows;
+using System.Windows.Input;
+using TorrentIsland.Presentation.Player.Scripts;
+using static TorrentIsland.Presentation.Player.Scripts.Limao;
+
+namespace TorrentIsland.Presentation.Player;
+
+public partial class TorrentSearchWindow : Wpf.Ui.Controls.FluentWindow
+{
+    public string? LinkSelecionado { get; private set; }
+
+    public TorrentSearchWindow()
+    {
+        InitializeComponent();
+    }
+
+    private async void BtnBuscar_Click(object sender, RoutedEventArgs e)
+    {
+        string query = TxtSearch.Text.Trim();
+        if (string.IsNullOrEmpty(query)) return;
+
+        TxtSearch.IsEnabled = false;
+        try
+        {
+            var resultado = await SearchAsync(query);
+            ListBoxTorrents.ItemsSource = resultado;
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Erro na busca: {ex.Message}", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        finally
+        {
+            TxtSearch.IsEnabled = true;
+        }
+    }
+
+    private void ListBoxTorrents_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+    {
+        if (ListBoxTorrents.SelectedItem is TorrentSearchDto torrent)
+        {
+            LinkSelecionado = torrent.LinkDownload;
+
+            DialogResult = true;
+            Close();
+        }
+    }
+
+    private void TxtSearch_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            BtnBuscar_Click(sender, e);
+        }
+    }
+}
